9a2a20a00785eecac19e9e568c7164c8d3083249
Leevi Graham <leevi@newism.com.au>
2010-06-15 16:51:51 +1000

    Added versions url which currently points at githubs master branch

--- config.php
+++ config.php
@@ -2,4 +2,4 @@
 
 $config['name']    = 'NSM TinyMCE';
 $config['version'] = '1.0.0RC2';
-$config['nsm_addon_updater']['versions_xml'] = '';
+$config['nsm_addon_updater']['versions_xml'] = 'http://github.com/newism/nsm.tiny_mce.ee_addon/raw/master/versions.xml';
