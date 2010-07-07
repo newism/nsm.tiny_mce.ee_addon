0287948ef21dad91ab4ba3a4a5a7b9c9a0836900
Leevi Graham <leevi@newism.com.au>
2010-07-07 16:12:45 +1000

    Code comment updates

--- ft.nsm_tiny_mce.php
+++ ft.nsm_tiny_mce.php
@@ -5,8 +5,8 @@
  *
  * @package			NsmTinyMCE
  * @version			1.0.0
- * @author			Leevi Graham <http://leevigraham.com>
- * @copyright 		Copyright (c) 2007-2010 Newism
+ * @author			Leevi Graham <http://leevigraham.com> - Technical Director, Newism
+ * @copyright 		Copyright (c) 2007-2010 Newism <http://newism.com.au>
  * @license 		Commercial - please see LICENSE file included with this distribution
  * @link			http://expressionengine-addons.com/nsm-tiny-mce
  * @see				http://expressionengine.com/public_beta/docs/development/fieldtypes.html
--- themes/third_party/nsm_tiny_mce/scripts/matrix2.js
+++ themes/third_party/nsm_tiny_mce/scripts/matrix2.js
@@ -3,9 +3,9 @@
  *
  * @package			NSMTinyMCE
  * @version			1.0.0
- * @author			Leevi Graham <leevi@newism.com.au>
+ * @author			Leevi Graham <http://leevigraham.com> - Technical Director, Newism
  * @link			http://github.com/newism/nsm.tiny_mce.ee_addon
- * @copyright 		Copyright (c) 2007-2010 Newism
+ * @copyright 		Copyright (c) 2007-2010 Newism <http://newism.com.au>
  * @license 		Commercial - please see LICENSE file included with this distribution
  */
 (function($) {
