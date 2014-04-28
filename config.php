80ff8490336ad348a7febb69fedee90f4508adaf
Iain Saxon <iain.saxon@newism.com.au>
2014-04-29 09:00:48 +1000

    Bumped version to 1.2.0

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
  * Config file for NSM TinyMCE
  *
  * @package			NsmTinyMCE
- * @version			1.1.1
+ * @version			1.2.0
  * @author			Leevi Graham <http://leevigraham.com> - Technical Director, Newism
  * @copyright 		Copyright (c) 2007-2010 Newism <http://newism.com.au>
  * @license 		Commercial - please see LICENSE file included with this distribution
@@ -11,5 +11,5 @@
  * @see				http://ee-garage.com/nsm-addon-updater
  */
 $config['name']    = 'NSM TinyMCE';
-$config['version'] = '1.1.1';
+$config['version'] = '1.2.0';
 $config['nsm_addon_updater']['versions_xml'] = 'http://ee-garage.com/nsm-tinymce/release-notes/feed';
